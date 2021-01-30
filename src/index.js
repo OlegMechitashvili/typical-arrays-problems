f60a604fdde2296605a72af2434afc4fac7454bc
OlegM71 <olegdrb@gmail.com>
2021-01-31 00:16:36 +0200

    test done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+   if (!arr || arr.length == 0) {
+      return 0;
+   } else {
+      function compNum (a, b) {return a - b};
+      return arr.sort(compNum)[0];
+   }   
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (!arr || arr.length == 0) {
+      return 0;
+   } else {
+      function compNum (a, b) {return a - b};
+      return arr.sort(compNum)[arr.length - 1];
+   }   
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+   let sum = 0;
+  if (!arr || arr.length == 0) {
+      return 0;
+   } else {
+      return arr.reduce((a, b) => a + b) / arr.length;
+    }   
 }
